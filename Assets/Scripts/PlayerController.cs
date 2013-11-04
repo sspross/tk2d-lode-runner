59448363d86fd08a193044b784f2a502d55be481
Silvan Spross <silvan.spross@gmail.com>
2013-11-05 00:48:29 +0100

    fixed shooting animation and added falling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
-	private float speed = 100F;
+	private float speed = 130F;
 	private float velocityX, velocityY = 0F;
 	private CharacterController characterController;
 	private tk2dSpriteAnimator spriteController;
-	private tk2dSpriteAnimator leftShootSprite, rightShootSprite;
 	private Vector3 moveDirection = Vector3.zero;
 	
 	private bool lookRight = true;
 	private bool onLadder = false;
 	private bool onRope = false;
+	private bool isShooting = false;
+	private bool isFalling = false;
 	
 	private int ladderTriggerCount, ropeTriggerCount = 0;
+	
+	private Transform shootParent;
+	private Renderer shootRenderer;
+	private tk2dSpriteAnimator shootSprite;
 
 
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
-		spriteController = GetComponent<tk2dSpriteAnimator>(); ;
-		leftShootSprite = transform.Find("leftShooter").GetComponent<tk2dSpriteAnimator>();
-		rightShootSprite = transform.Find("rightShooter").GetComponent<tk2dSpriteAnimator>();
-		leftShootSprite.gameObject.SetActive(false);
-		rightShootSprite.gameObject.SetActive(false);
+		spriteController = GetComponent<tk2dSpriteAnimator>();
+		shootParent = transform.Find("shoot parent");
+		shootRenderer = GameObject.Find("shoot").renderer;
+		shootSprite = GameObject.Find("shoot").GetComponent<tk2dSpriteAnimator>();
 	}
 	
 	// Update is called once per frame
@@ -37,41 +41,42 @@ public class PlayerController : MonoBehaviour {
 	
 	void InputCheck () {
 		velocityX = Input.GetAxis("Horizontal");
+		velocityY = Input.GetAxis("Vertical");
 		if (velocityX > 0) {
 			lookRight = true;
 		}
 		if (velocityX < 0) {
 			lookRight = false;
 		}
-		velocityY = Input.GetAxis("Vertical");
+		
+		// shooting
 		if (Input.GetKeyDown("space") && !onRope && !onLadder) {
-			if (lookRight) {
-				rightShootSprite.gameObject.SetActive(true);
-				rightShootSprite.Play();
-				rightShootSprite.gameObject.SetActive(false);
-			} else {
-				leftShootSprite.gameObject.SetActive(true);
-				leftShootSprite.Play();
-			}
+			StartCoroutine(Shoot());
 		}
 	}
 	
 	void Move () {
-		if(onLadder) 
-		{
-			moveDirection = new Vector3(velocityX, velocityY, 0f);
-			moveDirection *= Time.deltaTime * speed;
-		} else if (onRope) {
-			moveDirection = new Vector3(velocityX, 0f, 0f);
-			moveDirection *= Time.deltaTime * speed;
-			if (velocityY < 0) {
-				onRope = false;	
+		if (!isShooting) {
+			if (onLadder) {
+				moveDirection = new Vector3(velocityX, velocityY, 0f);
+				moveDirection *= Time.deltaTime * speed;
+			} else if (onRope) {
+				moveDirection = new Vector3(velocityX, 0f, 0f);
+				moveDirection *= Time.deltaTime * speed;
+				if (velocityY < 0) {
+					onRope = false;	
+				}
+			} else {
+				if (!characterController.isGrounded) {
+					isFalling = true;
+				} else {
+					isFalling = false;
+				}
+				moveDirection = new Vector3(velocityX, -1.5f, 0f);
+				moveDirection *= Time.deltaTime * speed;
 			}
-		} else {
-			moveDirection = new Vector3(velocityX, -1f, 0f);
-			moveDirection *= Time.deltaTime * speed;
+			characterController.Move(moveDirection);
 		}
-		characterController.Move(moveDirection);
 	}
 	
 	void SetAnimation () {
@@ -98,14 +103,20 @@ public class PlayerController : MonoBehaviour {
 			if (velocityX == 0) {
 				spriteController.Stop();
 			}
+		} else if (isFalling) {
+			if (lookRight) {
+				spriteController.Play("fallRight");
+			} else {
+				spriteController.Play("fallLeft");
+			}
 		} else {
-			if (velocityX > 0) {
+			if (velocityX > 0 && !isShooting) {
 				spriteController.Play("walkRight");
 			}
-			if (velocityX < 0) {
+			if (velocityX < 0 && !isShooting) {
 				spriteController.Play("walkLeft");
 			}
-			if (velocityX == 0) {
+			if (velocityX == 0 || isShooting) {
 				if (lookRight) {
 					spriteController.Play("stayRight");
 				} else {
@@ -146,6 +157,26 @@ public class PlayerController : MonoBehaviour {
 				onRope = false;
 			}
 		}
+	}
+	
+	IEnumerator Shoot()
+	{	
+		isShooting = true;
+		shootRenderer.enabled = true;
+		shootSprite.Play("shoot");
+		
+		// check facing direction and flip the shoot parent to the correct side
+		if (lookRight) {
+			shootParent.localPosition = new Vector3(30, 0, 0);
+			shootParent.localScale = new Vector3(-1, 1, 1); // right side
+		} else {
+			shootParent.localPosition = new Vector3(0, 0, 0);
+			shootParent.localScale = new Vector3(1, 1,1); // left side
+		}
+		
+		yield return new WaitForSeconds(0.5f);
+		shootRenderer.enabled = false;
+		isShooting = false;
 	}
 
 }
